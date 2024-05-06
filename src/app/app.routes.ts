96a1279312416efdb96a3d827361fadbd3bdcfb6
MiguelLopezPazos <ultravictor05@gmail.com>
2024-05-06 13:47:31 +0200

    Enrutamiento y Header, Navbar puestos con Login al inicio nada más iniciar la página

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { APIComponent } from './api/api.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'login',pathMatch:'full'},
+    {path:'login',component: LoginComponent},
+    {path:'inicio',component: InicioComponent},
+    {path:'API',component: APIComponent},
+    {path:'contacto',component: ContactoComponent},
+    {path:'header',component: HeaderComponent},
+    {path:'navbar',component: NavbarComponent}
+];
