29a361c490ec47c5b5905b6ac9b5fd6bb30a1bba
MiguelLopezPazos <ultravictor05@gmail.com>
2024-05-11 13:33:02 +0200

    apiideahecha

--- src/app/components/api/api.component.ts
+++ src/app/components/api/api.component.ts
@@ -2,11 +2,12 @@ import { DataDS } from './../../common/data-ds';
 import { Component, inject } from '@angular/core';
 import { DataServiceService } from '../../services/data.service.service';
 import { Data } from '@angular/router';
+import { NavbarComponent } from '../../layouts/navbar/navbar.component';
 
 @Component({
   selector: 'app-api',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './api.component.html',
   styleUrl: './api.component.css'
 })
